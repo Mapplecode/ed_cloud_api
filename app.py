5f75e4eed12963288b0afa1ad71eba77d9c68f31
Sparsh <mapplecode2020@gmail.com>
2021-05-18 02:18:26 +0530

    redirect

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ def feeds(my_key,value1,value2,media_Id,no_of_stories,from_date_start,till_date_
 
 @app.route('/')
 def hello_world():
-    return render_template('url_data.html')
+    return redirect('/get_date')
 
 
 @app.route('/get_data',methods = ['GET','POST'])
